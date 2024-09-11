d2faa1d69da9795dae2406d1c61d5f1dd174caea
salimov333 <salimov333@gmail.com>
2024-09-11 21:48:07 +0200

    deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-console.log("process.env.REACT_APP_DEPLOY_ENV", process.env.REACT_APP_DEPLOY_ENV);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.REACT_APP_DEPLOY_ENV === 'gh-pages' ? '/react-todo-app-vite/' : '/',
+  base: process.env.VITE_DEPLOY_ENV === 'gh-pages' ? '/react-todo-app-vite/' : '/',
 })
