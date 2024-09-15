57d3cbce53b8e5cfcad1076f165e5c068d2dcb54
salimov333 <salimov333@gmail.com>
2024-09-15 21:36:27 +0200

    add workflow_dispatch to deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+console.log("process.env.VITE_DEPLOY_ENV", process.env.VITE_DEPLOY_ENV);
 
 // https://vitejs.dev/config/
 export default defineConfig({
